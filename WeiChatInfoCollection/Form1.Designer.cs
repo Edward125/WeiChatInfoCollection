bfb2d4cf1dab6be6a4d59d05427b61bc9b46b3be
Edward <edward_song@yeah.net>
2019-08-22 16:32:05 +0800

    create..

---
+++ WeiChatInfoCollection/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WeiChatInfoCollection
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtWebLink = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboPlant = new System.Windows.Forms.ComboBox();
+            this.richMessage = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start Time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "End Time:";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpStart.Location = new System.Drawing.Point(88, 15);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(149, 22);
+            this.dtpStart.TabIndex = 2;
+            this.dtpStart.ValueChanged += new System.EventHandler(this.dtpStart_ValueChanged);
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpEnd.Location = new System.Drawing.Point(318, 15);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(143, 22);
+            this.dtpEnd.TabIndex = 3;
+            this.dtpEnd.ValueChanged += new System.EventHandler(this.dtpEnd_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtWebLink);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtEnd);
+            this.groupBox1.Controls.Add(this.txtStart);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboPlant);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dtpEnd);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtpStart);
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(705, 97);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setting";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(617, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 37);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtWebLink
+            // 
+            this.txtWebLink.Location = new System.Drawing.Point(88, 69);
+            this.txtWebLink.Name = "txtWebLink";
+            this.txtWebLink.ReadOnly = true;
+            this.txtWebLink.Size = new System.Drawing.Size(509, 22);
+            this.txtWebLink.TabIndex = 8;
+            this.txtWebLink.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 14);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Web Link:";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(318, 43);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.ReadOnly = true;
+            this.txtEnd.Size = new System.Drawing.Size(143, 22);
+            this.txtEnd.TabIndex = 6;
+            this.txtEnd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(88, 43);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.ReadOnly = true;
+            this.txtStart.Size = new System.Drawing.Size(149, 22);
+            this.txtStart.TabIndex = 5;
+            this.txtStart.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Plant:";
+            // 
+            // comboPlant
+            // 
+            this.comboPlant.FormattingEnabled = true;
+            this.comboPlant.Items.AddRange(new object[] {
+            "WCD",
+            "F721",
+            "F722",
+            "F230"});
+            this.comboPlant.Location = new System.Drawing.Point(512, 15);
+            this.comboPlant.Name = "comboPlant";
+            this.comboPlant.Size = new System.Drawing.Size(85, 22);
+            this.comboPlant.TabIndex = 4;
+            this.comboPlant.Text = "F721";
+            this.comboPlant.SelectedIndexChanged += new System.EventHandler(this.comboPlant_SelectedIndexChanged);
+            // 
+            // richMessage
+            // 
+            this.richMessage.Location = new System.Drawing.Point(22, 115);
+            this.richMessage.Name = "richMessage";
+            this.richMessage.Size = new System.Drawing.Size(705, 164);
+            this.richMessage.TabIndex = 5;
+            this.richMessage.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 291);
+            this.Controls.Add(this.richMessage);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboPlant;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWebLink;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox richMessage;
+    }
+}
+
---
+++ WeiChatInfoCollection/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WeiChatInfoCollection
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region 參數
+
+        
+        static string StartTime = "1556467200000";
+        static string EndTime = "1556553600000";
+        static string Plant = "F721";
+        static string WebLink = @"http://webap01.wks.wistron.com.cn:3010/api/DmcEvents?filter[where][STime][between][0]=" + @StartTime + @"&filter[where][STime][between][1]=" + @EndTime + @"&filter[where][toDMC]=1&filter[where][plant]=" + @Plant;
+
+        #endregion
+
+        private void LoadUI()
+        {
+            txtWebLink.Text = WebLink;
+            dtpStart.Value = Convert.ToDateTime(DateTime.Now.AddDays(-7).ToShortDateString() + " 00:00:00");
+            dtpEnd.Value = Convert.ToDateTime(DateTime.Now.ToShortDateString() + " 00:00:00");
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            LoadUI();
+        }
+
+
+        #region other
+
+
+
+        public enum MeaageType
+        {
+            Begin,
+            Success,
+            Failure,
+        }
+
+
+
+        private void ShowMessageInternal(MeaageType status, string message)
+        {
+
+            if (message == null)
+                message = status.ToString();
+            this.Invoke((EventHandler)(delegate
+            {
+                switch (status)
+                {
+                    case MeaageType.Begin:
+                        this.richMessage.SelectionColor = Color.Blue;
+                        this.richMessage.AppendText(DateTime.Now.ToString("HH:mm:ss") + " " + message + "\n");
+                        this.richMessage.Update();
+                        break;
+                    case MeaageType.Success:
+                        this.richMessage.SelectionColor = Color.Green;
+                        this.richMessage.AppendText(DateTime.Now.ToString("HH:mm:ss") + " " + message + "\n");
+                        this.richMessage.Update();
+                        break;
+                    case MeaageType.Failure:
+                        this.richMessage.SelectionColor = Color.Red;
+                        this.richMessage.AppendText(DateTime.Now.ToString("HH:mm:ss") + " " + message + "\n");
+                        this.richMessage.Update();
+                        break;
+                    default:
+                        break;
+                }
+                if (richMessage.TextLength > richMessage.MaxLength - 1000)
+                    richMessage.Clear();
+                richMessage.SelectionStart = richMessage.TextLength;
+                richMessage.ScrollToCaret();
+            }));
+
+        }
+
+
+
+        #endregion
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!CheckValue())
+                return;
+
+            
+
+        }
+
+
+
+        private bool CheckValue()
+        {
+            txtStart.Text = Local2Utc(dtpStart.Value);
+            StartTime = txtStart.Text.Trim();
+
+            if (string.IsNullOrEmpty(StartTime))
+            {
+                ShowMessageInternal(MeaageType.Failure, "Start time is null,pls retry...");
+                dtpStart.Focus();
+                return false;
+            }
+
+
+            txtEnd.Text = Local2Utc(dtpEnd.Value);
+            EndTime = txtEnd.Text.Trim();
+            if (string.IsNullOrEmpty(EndTime))
+            {
+                ShowMessageInternal(MeaageType.Failure, "End time is null,pls retry...");
+                dtpEnd.Focus();
+                return false;
+            }
+
+              
+
+
+            Plant = comboPlant.Text.Trim();
+            if (string.IsNullOrEmpty(Plant ))
+            {
+                ShowMessageInternal(MeaageType.Failure, "Plant is null,pls retry...");
+                comboPlant.Focus();
+                return false;
+            }
+
+
+
+            return true;
+        }
+
+
+        #region TransferTime
+
+
+
+        private string  Local2Utc(DateTime dt)
+        {
+            return ((dt.ToUniversalTime().Ticks - 0x89f7ff5f7b58000L )/ 0x989680L).ToString().PadRight(13, '0');
+
+        }
+
+        public string Transfer2UTC(DateTime dt)
+        {
+            //本地时间(北京时间)
+            //DateTime dt = Convert.ToDateTime("2019-01-01 00:00:00");
+
+            //将北京时间转换成utc时间 （北京时间是utc时间+8小时，所以此时utc时间应该是 2016-06-11 15:59:59）
+            DateTime utcNow = dt.ToUniversalTime();
+
+            //将utc时间转换成秒 (即将1970-01-01 00:00:00 到 2016-06-11 15:59:59的时间转换成秒)
+            double utc = ConvertDateTimeInt(utcNow);
+
+            //将秒数转换成北京时间 (其实就是将utc时间转换成北京时间),所以又得到2016-06-11 23:59:59
+            //DateTime dtime = ConvertIntDatetime(utc);
+
+            //return View();
+            return utc.ToString();
+            //return utc.ToString().PadRight(13, '0');
+        }
+
+        /// <summary>
+        /// 将时间转换成秒(这个秒是指1970-1-1 00:00:00 到你指定的时间之间的秒数)
+        /// </summary>
+        /// <param name="time">指定时间</param>
+        /// <returns>秒数</returns>
+        public double ConvertDateTimeInt(System.DateTime time)
+        {
+            double intResult = 0;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            intResult = (time - startTime).TotalSeconds;
+            return intResult;
+        }
+
+        /// <summary>
+        /// 将秒数转换成北京时间
+        /// </summary>
+        /// <param name="utc">秒数</param>
+        /// <returns>北京时间</returns>
+        public DateTime ConvertIntDatetime(double utc)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            startTime = startTime.AddSeconds(utc);
+            startTime = startTime.AddHours(8);//转化为北京时间(北京时间=UTC时间+8小时 )
+            return startTime;
+        }
+
+        
+    
+ 
+ 
+
+
+
+
+
+
+        #endregion
+
+        private void dtpStart_ValueChanged(object sender, EventArgs e)
+        {
+            txtStart.Text = Local2Utc(dtpStart.Value);
+            StartTime = txtStart.Text.Trim();
+            ShowMessageInternal(MeaageType.Begin, "Start Local Time:" + dtpStart.Value.ToString("yyyy-MM-dd HH:mm:ss") + ", UTC Time:" + StartTime);
+        }
+
+        private void dtpEnd_ValueChanged(object sender, EventArgs e)
+        {
+            txtEnd.Text = Local2Utc(dtpEnd.Value);
+            EndTime = txtEnd.Text.Trim();
+            ShowMessageInternal(MeaageType.Begin, "End Local Time:" + dtpEnd.Value.ToString("yyyy-MM-dd HH:mm:ss") + ", UTC Time:" + EndTime);
+        }
+
+        private void comboPlant_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Plant = comboPlant.Text;
+        }
+
+
+    }
+}
